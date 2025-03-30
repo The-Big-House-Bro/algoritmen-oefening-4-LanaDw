39075d0e476e14c219e540ebd8cedf5b2dae318f
Lana De Waele <dewaelelana@gmail.com>
2025-03-30 10:57:52 +0200

    Oefening 4

--- oefening4.py
+++ oefening4.py
@@ -1,1 +1,13 @@
-def countTargetPairs():
+def countTargetPairs(lijst):
+    aantalParen = 0
+    target = input(int())
+    for i in range (len(lijst)):
+        0 <= i < n 
+        for j in range (len(lijst)):
+             0 <= j < n and i != j
+             if nums[i] + nums[j] < target:
+                aantalParen =+ 1
+    return aantalParen
+
+
+
